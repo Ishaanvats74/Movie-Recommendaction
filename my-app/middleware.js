0b6610a9b67d1a167f55483e83047d1f1af366f0
Ishaanvats74 <Ishaanvats74@gmail.com>
2025-03-13 19:23:31 +0530

    .

--- my-app/middleware.js
+++ my-app/middleware.js
@@ -1,8 +1,8 @@
-import { clerkMiddleware } from "@clerk/nextjs";
+const { clerkMiddleware } = require("@clerk/nextjs");
 
-export default clerkMiddleware();
+module.exports = clerkMiddleware();
 
-export const config = {
+module.exports.config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
